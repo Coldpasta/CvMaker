5c8b5d93dd855e2011b22ff194f99747b068993c
Coldpasta <Lmacewicz@gmail.com>
2025-03-12 19:39:22 +0100

    Swapped H2 for Postgres database. Security Config changed to allow posting and getting from "/user" http calls

--- src/main/java/com/lm/cvmaker/config/SecurityConfig.java
+++ src/main/java/com/lm/cvmaker/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.lm.cvmaker.config;
 
 import com.lm.cvmaker.security.JwtFilter;
-import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
