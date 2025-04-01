b6eb8db4dfda3bfde5e87bf9572473df27a81485
Coldpasta <147087691+Coldpasta@users.noreply.github.com>
2025-04-01 14:06:48 +0200

    Update User.java

--- src/main/java/com/lm/cvmaker/model/User.java
+++ src/main/java/com/lm/cvmaker/model/User.java
@@ -17,7 +17,7 @@ public class User {
 
     private String name;
     private String email;
-    private String password; // Hash this before storing
+    private String password; 
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
